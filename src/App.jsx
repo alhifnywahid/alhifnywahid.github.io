fd8c6cb7dfc3ba114497b2fd5092997b5aa1ec06
alhifnywahid <hifnycarlytosss@gmail.com>
2024-06-18 13:27:05 +0700

    pembaruan

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen flex justify-center items-center flex-col bg-slate-900">
+        <img className="rounded-full" src="/profile.webp" alt="" />
+        <div className="text-3xl text-white my-10">Halo Dunia</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
